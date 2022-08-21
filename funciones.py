6cc271c7d756244a4ab05ef2156206948ed44b38
Barbero-Bautista <barberogaticabautista@gmail.com>
2022-08-20 23:36:47 -0300

    nuevas funciones

--- funciones.py
+++ funciones.py
@@ -1,47 +1,77 @@
 import pickle
-nombres = ['Pepe']
+
+usuario = ['Pepe']
 contrasenias = ['Pepe123']
-alumnos = [['Bauty', 'Barbero', ['Mate', 3, 'Lengua', 4] ]]
-mayusculas = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-contador_mayusculas = 0
+
+nombres = []
+padres = []
+notas = []
+kiosko = []
+
+arch_nombres = open('nombres.txt', 'w')
+arch_padres = open('padres.txt', 'w')
+arch_notas = open('notas.txt', 'w')
+arch_kiosko = open('kiosko.txt', 'w')
 
 
+def linea():
+    print('+' + '-' * 60 + '+')
 
 
 def opciones_iniciales():
-    print('Bienvenido a el programa seleccione una opcion:')
-    opciones = ['Agregar un alumno = 1', 'Sacar un alumno = 2', 'Gestionar Quiosco = 3' ]
+    linea()
+    print('|       Bienvenido a el programa seleccione una opcion       |')
+    linea()
+    opciones = ['Agregar alumno = 1', 'Sacar alumno = 2', 'Gestionar Quiosco = 3', 'Mostrar Alumnos = 4']
     for i in range(len(opciones)):
         print(opciones[i])
 
 
-
 def ingresar_nuevo_alumno():
-    datos = []
-    notas = []
-    nuevo_alumno = input('Ingrese el nombre y apellido del alumno\n>>>')
-    padre = input('Ingrese uno de sus padres\n>>>')
-    decicion = int(input('Ingrese la cantidad de notas que quiere ingresar\n>>>'))
-    for i in range(decicion):
-        materia = input('Ingrese el nombre de la materia\n>>>')
-        nota = int(input(f'Ingrese la nota de la materia {materia}\n>>>'))
+    nuevo_alumno = input('| Ingrese el nombre y apellido del alumno\n|>>>')
+    padre = input('| Ingrese uno de sus padres\n|>>>')
+    cant_notas = int(input('| Ingrese la cantidad de notas que quiere ingresar\n|>>>'))
+    for i in range(cant_notas):
+        materia = input('| Ingrese el nombre de la materia\n|>>>')
+        nota = int(input(f'| Ingrese la nota de la materia {materia}\n|>>>'))
         notas.append(materia)
         notas.append(nota)
-    datos.append(nuevo_alumno)
-    datos.append(padre)
-    datos.append(notas)
-    alumnos.append(datos)
-    pickle.dump(alumnos, open('Alumnos.txt', 'wb'))
+    nombres.append(nuevo_alumno)
+    padres.append(padre)
+    kiosko.append(0)
+
 
 def sacar_alumno():
-    cont = True
-    indice = 0
-    alumno = input('Ingrese el nombre y apellido del alumno\n>>>')
-    while cont:
-        if alumno in alumnos[indice][0]:
-            alumnos.pop(indice)
-            print(alumnos)
-            break
-        else:
-            print('Pasando')
-            indice += 1
+    alumno = input('Indique el nombre del alumno a sacar\n>>>')
+    for i in range(len(nombres) - 1, -1, -1):
+        aux = i + 1
+        if nombres[i] == alumno:
+            nombres.pop(i)
+            padres.pop(i)
+            notas.pop(i)
+            notas.pop(aux)
+            kiosko.pop(i)
+
+
+def mostrar_alumnos():
+    linea()
+    print('|Nombre         |Padre          |Deuda |')
+    for i in range(len(nombres)):
+        print('|{:<15}|{:<15}|{:<6}|'.format(nombres[i], padres[i], 0))
+
+
+def agregar_deuda():
+    pass
+
+
+def guardar_alumnos():
+    for t in range(len(nombres)):
+        arch_nombres.write(nombres[t] + ',')
+        arch_padres.write(padres[t] + ',')
+        arch_notas.write(notas[t] + ',')
+        arch_kiosko.write(kiosko[t] + ',')
+
+    arch_nombres.close()
+    arch_padres.close()
+    arch_notas.close()
+    arch_kiosko.close()
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-from functions import *
+from funciones import *
 
 
 intento = 0
-validacion_nombre = input('Ingrese su nombre de usuario\n>>>')
-for i in nombres:
-    if validacion_nombre == i:
-        while intento < 3:
-            validacion_contrasenia = input('Ingrese su contrasenia\n>>>')
-            for j in contrasenias:
-                if validacion_contrasenia == j:
-                    print('Inicio de sesion correcto')
-                    intento = 5
-                else:
-                    print('Contrasenia incorrecta')
-                    intento += 1
-                    intento_restantes = 3 - intento
-                    print(f'Te quedan {intento_restantes} intentos')
-                if intento == 3:
-                    break
-    else:
-        print('Nombre de usuario no identificdo')
+while intento != 5:
+    for i in usuario:
+        validacion_nombre = input('Ingrese su nombre de usuario\n>>>')
+        if validacion_nombre == i:
+            while intento < 3:
+                validacion_contrasenia = input('Ingrese su contrasenia\n>>>')
+                for j in contrasenias:
+                    if validacion_contrasenia == j:
+                        print('Inicio de sesion correcto')
+                        intento = 5
+                    else:
+                        print('Contrasenia incorrecta')
+                        intento += 1
+                        intento_restantes = 3 - intento
+                        print(f'Te quedan {intento_restantes} intentos')
+                    if intento == 3:
+                        break
+        else:
+            print('Nombre de usuario no identificdo')
 
 
-while True:
-    if intento == 5:
-        opciones_iniciales()
-        decicion = int(input('>>>'))
-        if decicion == 1:
-            ingresar_nuevo_alumno()
-        elif decicion == 2:
-            sacar_alumno()
-        elif decicion == 3:
-            alumnosss = pickle.load(open('Alumnos.txt', 'rb'))
-            print(alumnosss)
+while intento == 5:
+    opciones_iniciales()
+    decision = int(input('>>>'))
+    if decision == 1:
+        ingresar_nuevo_alumno()
+    elif decision == 2:
+        sacar_alumno()
+    elif decision == 3:
+        pass
+    elif decision == 4:
+        mostrar_alumnos()
+    elif decision == 5:
+        agregar_deuda()
+    elif decision == 6:
+        pass
+
+
